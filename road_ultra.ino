4fc54e3040acf3566e6064fb893712e0cf84f611
kirajkc <60384471+kirajkc@users.noreply.github.com>
2020-02-28 16:19:57 +0545

    Add files via upload

---
+++ road_ultra.ino
@@ -0,0 +1,192 @@
+#include <SoftwareSerial.h>
+SoftwareSerial gsm(2, 3);
+#define trigpin1 4 // digital pin 4 
+#define echopin1 7 // digital pin 3
+#define trigpin2 5
+#define echopin2 6 
+#define trigpin3 8
+#define echopin3 9
+ int currentState = 0,currentState2=0;
+ int previousState = 0,previousState2=0;
+ int duration1, distance1,duration2,duration3, distance2,c=0;
+
+void setup()
+{
+Serial.begin(9600);
+gsm.begin(9600);
+pinMode(trigpin1, OUTPUT);
+pinMode(echopin1, INPUT);
+ pinMode(trigpin2, OUTPUT);
+pinMode(echopin2, INPUT);
+ pinMode(trigpin3, OUTPUT);
+pinMode(echopin3, INPUT);
+
+
+}
+ 
+void loop()
+{
+  int p;
+if (Serial.available()>0)
+{
+  char n = Serial.read();
+  if ( n == 'a')
+  {
+ msg(77,1);
+ delay(100);
+
+  }
+ if (n == 's')
+ {
+ msg(77,2);
+ delay(100);
+
+ }
+}
+else{
+ ult1();
+ult2();
+ count();
+ p = ult3();
+ Serial.println(p);
+ Serial.print("count=");
+ Serial.print(c);
+ Serial.print("------------------");
+Serial.println();
+delay(100);
+}
+delay(1000);
+}
+
+/* ----------------------------Ultrasonic 1------------------------------*/
+void ult1()
+{
+  digitalWrite(trigpin1, HIGH);
+ 
+delayMicroseconds(1000);  
+digitalWrite(trigpin1, LOW);
+ 
+ 
+duration1 = pulseIn(echopin1,HIGH);
+ 
+distance1 = ( duration1 / 2) / 29.1;
+Serial.print("inches1:"); 
+Serial.println(distance1);
+ }
+
+ /* ----------------------------Ultrasonic 2------------------------------*/
+void ult2()
+{
+  digitalWrite(trigpin2, HIGH);
+ 
+delayMicroseconds(1000);  
+digitalWrite(trigpin2, LOW);
+ 
+ 
+duration2 = pulseIn(echopin2,HIGH);
+ 
+distance2 = ( duration2 / 2) / 29.1;
+Serial.print("inches2:"); 
+Serial.println(distance2);
+ }
+ /*--------------ult 3-----------------*/
+ int ult3()
+{
+  int distance3,l;
+  digitalWrite(trigpin3, HIGH);
+ 
+delayMicroseconds(1000);  
+digitalWrite(trigpin3, LOW);
+ 
+ 
+duration3 = pulseIn(echopin3,HIGH);
+ 
+distance3 = ( duration3 / 2) / 29.1;
+Serial.print("inches3:"); 
+Serial.println(distance3);
+if (distance3 <= 5)
+{ l=1;
+ Serial.println(l);}
+if (distance3 > 5)
+{
+ l=0;
+ Serial.println(l);
+}
+return l;
+ }
+ 
+/* ----------------------------Count Vehicle------------------------------*/
+ 
+void count()
+{
+  if ((distance1 <=5) && (distance1 !=0))
+  {
+ currentState = 1;
+ }
+ else {
+ currentState = 0;
+ }
+ 
+ delay(200);
+ if(currentState != previousState){
+ if(currentState == 1){
+ c = c + 1;
+
+ delay(1200);
+ }
+}
+
+if ((distance2 <=5) && (distance2 !=0))
+  {
+ currentState2 = 1;
+ }
+ else {
+ currentState2 = 0;
+ }
+ 
+ delay(200);
+ if(currentState2 != previousState2){
+ if(currentState2 == 1){
+ c = c - 1;
+
+ delay(1200);
+ }
+}
+}
+
+/* ----------------------------GSM module------------------------------*/
+
+void msg(int m, int l)
+
+{ 
+  if(m>70)
+  {
+    Serial.println("send");
+      gsm.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+     delay(1000);  // Delay of 1 second
+     if(l==1)
+     {Serial.println("senda");
+     gsm.println("AT+CMGS=\"+9779805968955\""); // Replace x with mobile number
+     delay(1000);
+     gsm.println("Accident occured at ");// The SMS text you want to send
+     delay(100);
+     gsm.println("Point B");// The SMS text you want to send
+     delay(100);
+      gsm.println((char)26);// ASCII code of CTRL+Z for saying the end of sms to  the module 
+      delay(1000);
+     }
+     if (l==2)
+     {Serial.println("sends");
+       gsm.println("AT+CMGS=\"+9779818200278\""); // Replace x with mobile number
+     delay(1000);
+     gsm.println("Accident occured at ");// The SMS text you want to send
+     delay(100);
+     gsm.println("Point A");// The SMS text you want to send
+     delay(100);
+      gsm.println((char)26);// ASCII code of CTRL+Z for saying the end of sms to  the module 
+      delay(1000);
+     }
+  
+      
+  }
+}
---
+++ vehihcle.ino
@@ -0,0 +1,178 @@
+
+ 
+ #include <SoftwareSerial.h>
+int vs =9; // vibration sensor
+SoftwareSerial Rfid = SoftwareSerial(2,3);
+
+
+
+int enb = 5; 
+
+
+int in3 = 12; 
+int in4 = 13; 
+
+int distancem; 
+void setup()
+{
+   Rfid.begin(9600);
+   pinMode(vs, INPUT);  
+  Serial.begin(9600);
+pinMode(A0,INPUT);
+  pinMode(enb, OUTPUT); 
+
+
+  pinMode(in3, OUTPUT); 
+  pinMode(in4, OUTPUT);
+
+  
+ analogWrite(enb, 0); 
+
+ delay(1000); // Setting the baud rate of Serial Monitor (Arduino)
+ 
+}
+
+
+void loop()
+{ 
+ 
+  if(Serial.available()>0)
+  {
+  char n=Serial.read();
+  if (n == 'q'||n == 'w')
+ control(n);
+ if (n=='s')
+ limit(n);
+ }
+ else{ 
+ 
+speedcontrol();
+ long measurement =vibration();
+  delay(50);
+  Serial.println(measurement);
+  if (measurement > 0)
+   rfid();
+ }
+   delay(1000);
+}
+
+/*-----------------------vibration sensor-------------------------*/
+long vibration(){
+ 
+  long measurement=pulseIn (vs, HIGH);  //wait for the pin to get HIGH and returns measurement
+
+  return measurement;
+}
+
+/*------------------Rfid location---------------------*/
+void rfid()
+{
+ int r;
+int c=0;
+int location = 0;
+if (Rfid.available()> 0){
+  while(Rfid.available() > 0){
+    int r =Rfid.read();
+    c=c+r;
+    
+    if (c==693 || c == 653 || c == 717)
+    break;
+    Serial.print(r,DEC );
+    }
+    Serial.println();
+Serial.println(c);
+  }
+if (c== 693)
+location =2;
+if (c == 653)
+location = 1;
+switch(location){
+  case 1:
+  Serial.println("Point A");
+  break;
+  case 2:
+  Serial.println("Point B");
+  break;
+}
+delay(100);
+}
+/*-------------------------Safe gap maintain-------------------*/
+void speedcontrol()
+{
+  int distance= analogRead(A0);
+//int distance = map(distancem, 0 , 1023, 0 ,150); 
+Serial.println("Distance = ");
+Serial.println(distance);
+delay(100);
+if(  (distance < 0)   ) 
+{
+distance = 0; 
+} else
+if(  (distance >= 0) && (distance<= 30)  ) 
+{
+  Serial.println("stop");
+  analogWrite(enb, 0); 
+
+    digitalWrite(in3, LOW); 
+  digitalWrite(in4, LOW);
+  
+} else
+
+if(  distance > 30 ) 
+{
+  Serial.println("go");
+  analogWrite(enb, 255); 
+ 
+    digitalWrite(in3, HIGH); 
+  digitalWrite(in4, LOW);
+ 
+}
+ }
+/*----------------------Speed control heavy road----------------*/
+void control(char d)
+{
+  if (d == 'q') 
+{
+  analogWrite(enb, 110); 
+    digitalWrite(in3, HIGH); 
+  digitalWrite(in4, LOW);
+  Serial.println("Heavy");
+  Serial.println();
+} 
+
+if(  d == 'w' ) 
+{
+  analogWrite(enb,255); 
+    digitalWrite(in3, HIGH); 
+  digitalWrite(in3, LOW);
+  Serial.println("gogogog");
+}
+
+}
+/*-------------------------------Speed Limit Area-----------------*/
+ void limit(char p)
+ {
+ 
+  if(  p=='s'  ) 
+{
+  analogWrite(enb, 100); 
+    digitalWrite(in3, HIGH); 
+  digitalWrite(in4, LOW);
+  Serial.println("speed limit");
+  Serial.println();
+}
+else 
+{
+ 
+   analogWrite(enb, 255); 
+    digitalWrite(in3, HIGH); 
+  digitalWrite(in4, LOW);
+
+
+
+}
+
+}
+/*------------------*/
+
+ 
